c326cedbcfa566f3911e384138d7e9e472d43d6e
Iago Dantas <iagodantas.8@gmail.com>
2023-02-02 14:23:29 -0300

    view model and mapper to notification

--- src/infra/http/controllers/notifications.controller.ts
+++ src/infra/http/controllers/notifications.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller,Post } from '@nestjs/common';
 import { CreateNotificationBody } from '../dtos/create-notification-body';
 import { SendNotification } from '@application/use-cases/send-notification';
+import { NotificationViewModel } from '../view-models/notification-view-model';
 
 
 @Controller('notifications')
@@ -21,6 +22,8 @@ export class NotificationsController {
       content
     });
 
-    return { notification };
+    return { 
+      notification:NotificationViewModel.toHTTP(notification)
+    };
   }
 }
